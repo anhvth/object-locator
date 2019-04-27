9a832cfe4124f71c8924fa47b330051e2e393f55
Tran Hai Anh Vo <vnbot2@gmail.com>
2019-04-27 15:57:19 +0700

    remove hard-coded args with argumentparse

--- train.py
+++ train.py
@@ -6,38 +6,49 @@ import pandas
 import logging
 import os
 import cv2
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--mode', default='train', choices=['train', 'debug'], 
+		help='if mode is debug then init small net with 1 batch example to test')
+parser.add_argument('--height', default=256, type=int, help='image height')
+parser.add_argument('--width', default=256, type=int, help='image width')
+parser.add_argument('--lr', default=4e-4, help='learning rate')
+parser.add_argument('--p', default=-3, help='UNKNOWN ')
+parser.add_argument('--max_epochs', default=int(10e5), help='num of max epoch ')
+parser.add_argument('--output_dir', default='training_checkpoints', help='directory to save model weights')
+parser.add_argument('--nClass', default=1, help='num of output channels')
+parser.add_argument('--batch_size', default=2, help='batch size')
+parser.add_argument('--max_to_keep', default=2, help='batch size')
+
+
+
+args = parser.parse_args()
 
 logger = logging.getLogger()
-#logger.setLevel(level=logging.DEBUG)
 print('version: {}'.format(tf.__version__))
 
 if __name__ == "__main__":
-    nClass = 1
-    height = 256
-    width = 256
-    batch_size = 2
-    p = -3
-    lr = 4e-4
-    max_epochs = int(10e5)
-    output_dir = 'samples_images/'
-    checkpoint_dir = 'checkpoints/'
+    output_dir = os.path.join(args.output_dir, 'samples_images')
+    checkpoint_dir = os.path.join(args.output_dir, 'checkpoints')
     os.makedirs(output_dir, exist_ok=True)
     os.makedirs(checkpoint_dir, exist_ok=True)
 
-    model = unet_model.UNet(nClass, height, width)
+    model = unet_model.UNet(args.nClass, args.height, args.width)
     loss_loc = losses.WeightedHausdorffDistance(
-        height, width, p=p, return_2_terms=True)
+        args.height, args.width, p=args.p, return_2_terms=True)
     # dataset
     df = pandas.read_json('./datasets/train/train.json')
-    # df = df[:batch_size]
-    dataset = data.create_dataset(df, batch_size, height, width)
+    # df = df[:args.batch_size]
+    dataset = data.create_dataset(df, args.batch_size, args.height, args.width)
     dataset = dataset.prefetch(100)
-    iter = dataset.repeat(max_epochs).make_one_shot_iterator()
+    iter = dataset.repeat(args.max_epochs).make_one_shot_iterator()
 
     imgs, locs, orig_sizes = iter.get_next()
     # train ops
     prob_map = model(imgs)
-    optimizer = tf.train.AdamOptimizer(lr, use_locking=True)
+    optimizer = tf.train.AdamOptimizer(args.lr, use_locking=True)
 
     term1, term2 = loss_loc(prob_map, locs, orig_sizes)
     loss = term1 + term2
@@ -45,13 +56,13 @@ if __name__ == "__main__":
 
     train_step = optimizer.minimize(loss, global_step=global_step)
 
-    saver = tf.train.Saver(max_to_keep=2)
+    saver = tf.train.Saver(max_to_keep=args.max_to_keep)
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    steps_per_epoch = len(df)// batch_size
+    steps_per_epoch = len(df)// args.batch_size
 
-    for epoch in range(max_epochs):
+    for epoch in range(args.max_epochs):
         for step in range(steps_per_epoch):
             _imgs, _prob_map, _t1, _t2, _= sess.run([imgs, prob_map, term1, term2, train_step])
             print('{}:{}-Term1: {:0.5f}, Term2:{:0.5f}'.format(epoch, step, _t1, _t2))
